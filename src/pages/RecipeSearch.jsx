dd49b10aed1a61ebaba71c7ca07ebcd5ec78b134
Junier Bolivar <junierba@yahoo.co.uk>
2024-04-03 12:21:05 +0200

    change data fetch path

--- src/pages/RecipeSearch.jsx
+++ src/pages/RecipeSearch.jsx
@@ -8,31 +8,29 @@ export const RecipeSearch = ({ clickFn }) => {
   const [recipes, setRecipes] = useState([]); // State to store the fetched recipes
 
   useEffect(() => {
-    fetch('http://localhost:3001/data')
-    .then(response => response.json())
-    .then(data => {
-      setRecipes(data.hits); // Adjust based on your JSON structure
-    })
-    .catch(error => console.error('Error fetching data:', error));
-  
+    fetch('http://localhost:3000/data')
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data.hits); // Adjust based on your JSON structure
+      })
+      .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   const handleChange = (event) => {
     setSearchField(event.target.value);
   };
 
-  const matchedRecipes = recipes.filter(hit =>
-    hit.recipe.label.toLowerCase().includes(searchField.toLowerCase())
-  );
+  const matchedRecipes = recipes.filter((hit) => hit.recipe.label.toLowerCase().includes(searchField.toLowerCase()));
 
   return (
     <>
       <Center display="flex" flexDir="column">
-        <Heading size="lg" marginBottom="1rem">Search for recipes</Heading>
+        <Heading size="lg" marginBottom="1rem">
+          Search for recipes
+        </Heading>
         <TextInput changeFn={handleChange} mb={8} />
         <RecipeListPage clickFn={clickFn} recipes={matchedRecipes} />
       </Center>
     </>
   );
 };
-
