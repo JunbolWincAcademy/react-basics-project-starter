fdca6cf498d121d2139fa58845c7ed5786cdb884
Junier Bolivar <junierba@yahoo.co.uk>
2024-06-20 22:32:27 +0200

    add base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/react-basics-project-starter/', //✅ this was added to correct the wrong index.html issue
   plugins: [react()],
 });
-
